1015a57d7e946175c89636237df2420dd968ea57
Nikolaus Mayer <mayern@cs.uni-freiburg.de>
2020-12-22 16:07:27 +0100

    Day 15 Python

---
+++ day-15/python/part-1.py
@@ -0,0 +1,35 @@
+import re
+import sys
+
+RE = re.compile(r'-?\d+')
+
+ingredients = []
+for line in open(sys.argv[1]).readlines():
+    ingredients.append([int(v) for v in RE.findall(line)])
+
+def score(amounts):
+    negative = False
+    product = 1
+    for i in range(len(ingredients[0])-1):
+        iscore = sum([amounts[j]*ingredients[j][i] for j in range(len(ingredients))])
+        product *= abs(iscore)
+        if iscore < 0:
+            negative = True
+    return -product if negative else product
+
+maxscore = 0
+
+iter = 0
+for a in range(101):
+    for b in range(101):
+        for c in range(101):
+            for d in range(101):
+                iter += 1
+                #if iter % 1000000 == 0:
+                #    print(iter, maxscore)
+                if a+b+c+d != 100:
+                    continue
+                maxscore = max(score([a,b,c,d]), maxscore)
+
+print(maxscore)
+
